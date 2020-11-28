8ce8f8c97ae1765aedb13d723002351671f44117
Eunice Oh <euniceyoh@gmail.com>
2020-11-27 17:25:39 -0800

    made status class

--- src/application/Main.java
+++ src/application/Main.java
@@ -30,12 +30,13 @@ public class Main extends Application {
 		  // create the nodes
 		  BorderPane root = new BorderPane();
 		  ToolBar toolbar = constructToolBar();
-		  HBox hbox = constructHBox(primaryStage);
+		  Status s = new Status();
+		  HBox statusBox = s.constructStatus(primaryStage);
 		  
 		  root.setTop(toolbar);
-		  root.setCenter(hbox);
+		  root.setCenter(statusBox);
 		  BorderPane.setAlignment(toolbar, Pos.CENTER);
-		  BorderPane.setAlignment(hbox, Pos.CENTER);
+		  BorderPane.setAlignment(statusBox, Pos.CENTER);
 		  
 		  Scene scene = new Scene(root,600,600);
 		  primaryStage.setScene(scene);
@@ -55,59 +56,6 @@ public class Main extends Application {
 		
 		return toolbar;
 	}
-
-	// makes the horizontal box 
-	// there are three v boxes, one for todo, one for doing, and one for done
-	private HBox constructHBox(Stage primaryStage) {
-		// leave the buttons alone
-		Button addTask1 = new Button("+");
-		Button addTask2 = new Button("+");
-		Button addTask3 = new Button("+");
-		
-		// Define an event handler
-		EventHandler handler = new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                final Stage dialog = new Stage();
-                dialog.initModality(Modality.APPLICATION_MODAL);
-                dialog.initOwner(primaryStage);
-                VBox dialogVbox = new VBox(20);
-                dialogVbox.getChildren().add(new Label("Add Assignment"));
-                Scene dialogScene = new Scene(dialogVbox, 300, 200);
-                dialog.setScene(dialogScene);
-                dialog.show();
-            }
-         };
-		
-		// makes another window popup after clicking the first plus button
-	    addTask1.setOnAction(handler);
-	    
-		// makes another window popup after clicking the second plus button
-	    addTask2.setOnAction(handler);
-	    
-		// makes another window popup after clicking the third plus button
-	    addTask3.setOnAction(handler);
-	    
-	    VBox vbox1 = new VBox(10); // spacing = 8
-	    vbox1.getChildren().addAll(new HBox(new Label("TODO     "), addTask1), new Label("code todo list"), new Label("do history homework"));
-	    
-	    VBox vbox2 = new VBox(10); // spacing = 8
-	    vbox2.getChildren().addAll(new HBox(new Label("DOING     "), addTask2), new Label("code todo list"), new Label("do history homework"));
-
-	    VBox vbox3 = new VBox(10); // spacing = 8
-	    vbox3.getChildren().addAll(new HBox(new Label("DONE     "), addTask3), new Label("code todo list"), new Label("do history homework"));
-	    
-        Region region1 = new Region();
-        HBox.setHgrow(region1, Priority.ALWAYS);
-
-        Region region2 = new Region();
-        HBox.setHgrow(region2, Priority.ALWAYS);
-
-        HBox hbox = new HBox(vbox1, region1, vbox2, region2, vbox3);
-
-		return hbox;
-	}
-	
 	
   public static void main(String[] args) {
 		launch(args);
---
+++ src/application/Status.java
@@ -0,0 +1,69 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Status {
+	
+	// makes the horizontal box 
+	// there are three v boxes, one for todo, one for doing, and one for done
+	public HBox constructStatus(Stage primaryStage) {
+		// leave the buttons alone
+		Button addTask1 = new Button("+");
+		Button addTask2 = new Button("+");
+		Button addTask3 = new Button("+");
+		
+		// Define an event handler
+		EventHandler handler = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                final Stage dialog = new Stage();
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                dialog.initOwner(primaryStage);
+                VBox dialogVbox = new VBox(20);
+                dialogVbox.getChildren().add(new Label("Add Assignment"));
+                Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                dialog.setScene(dialogScene);
+                dialog.show();
+            }
+         };
+		
+		// makes another window popup after clicking the first plus button
+	    addTask1.setOnAction(handler);
+	    
+		// makes another window popup after clicking the second plus button
+	    addTask2.setOnAction(handler);
+	    
+		// makes another window popup after clicking the third plus button
+	    addTask3.setOnAction(handler);
+	    
+	    VBox vbox1 = new VBox(10); // spacing = 8
+	    vbox1.getChildren().addAll(new HBox(new Label("TODO"), addTask1), new Label("code todo list"), new Label("do history homework"));
+	    
+	    VBox vbox2 = new VBox(10); // spacing = 8
+	    vbox2.getChildren().addAll(new HBox(new Label("DOING"), addTask2), new Label("code todo list"), new Label("do history homework"));
+
+	    VBox vbox3 = new VBox(10); // spacing = 8
+	    vbox3.getChildren().addAll(new HBox(new Label("DONE"), addTask3), new Label("code todo list"), new Label("do history homework"));
+	    
+        Region region1 = new Region();
+        HBox.setHgrow(region1, Priority.ALWAYS);
+
+        Region region2 = new Region();
+        HBox.setHgrow(region2, Priority.ALWAYS);
+
+        HBox hbox = new HBox(vbox1, region1, vbox2, region2, vbox3);
+
+		return hbox;
+	}
+
+}

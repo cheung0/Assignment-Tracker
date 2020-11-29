c36d69885f32f158f18e93ef6dc730e8affe8d6f
Eunice Oh <euniceyoh@gmail.com>
2020-11-29 01:12:39 -0800

    updated interface, can add and delete items for the different columns

--- src/application/HomeToolBar.java
+++ src/application/HomeToolBar.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 
 public class HomeToolBar {
 	
@@ -21,11 +22,13 @@ public class HomeToolBar {
 	public HBox constructTopBar() {
 		HBox hb = new HBox(); 
 		
-		hb.setPadding(new Insets(20,20,20,20));		
+		hb.setPadding(new Insets(20,20,10,20));		
 		
 		hb.setStyle("-fx-background-color: #89cff0;");
 		
 		Label title = new Label("Assignment Tracker");
+		title.setFont(new Font("Arial", 20));
+		
 			
 		hb.getChildren().add(title);
 		
--- src/application/Status.java
+++ src/application/Status.java
@@ -1,72 +1,142 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Status {
 	
-	// makes the horizontal box 
-	// there are three v boxes, one for todo, one for doing, and one for done
-	public HBox constructStatus(Stage primaryStage) {
-		// leave the buttons alone
-		Button addTask1 = new Button("+");
-		Button addTask2 = new Button("+");
-		Button addTask3 = new Button("+");
-		
-		// Define an event handler
-		EventHandler handler = new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-            	final Stage dialog = new Stage();
-                dialog.initModality(Modality.APPLICATION_MODAL);
-                dialog.initOwner(primaryStage);
-                
-                Task t = new Task(); 
-                VBox taskPopUp = t.constructTask();
-              
-                Scene dialogScene = new Scene(taskPopUp, 400, 400);
-                dialog.setScene(dialogScene);
-                dialog.show();
-            }
-         };
-		
-		// makes another window popup after clicking the first plus button
-	    addTask1.setOnAction(handler);
+	private static BorderPane todo;
+	private static BorderPane doing;
+	private static BorderPane done;
+
+	public HBox constructStatus() {
 	   
-	    addTask2.setOnAction(handler);
-	    
-	    addTask3.setOnAction(handler);
-	    
-	    VBox vbox1 = new VBox(10); // spacing = 8
-	    vbox1.getChildren().addAll(new HBox(new Label("TODO"), addTask1), new Label("code todo list"), new Label("do history homework"));
-	    
-	    VBox vbox2 = new VBox(10); // spacing = 8
-	    vbox2.getChildren().addAll(new HBox(new Label("DOING"), addTask2), new Label("code todo list"), new Label("do history homework"));
-
-	    VBox vbox3 = new VBox(10); // spacing = 8
-	    vbox3.getChildren().addAll(new HBox(new Label("DONE"), addTask3), new Label("code todo list"), new Label("do history homework"));
-	    
-        Region region1 = new Region();
-        HBox.setHgrow(region1, Priority.ALWAYS);
-
-        Region region2 = new Region();
-        HBox.setHgrow(region2, Priority.ALWAYS);
-
-        HBox hbox = new HBox(vbox1, region1, vbox2, region2, vbox3);
+	    todo = createTaskColumn("ToDo");
+	    doing = createTaskColumn("Doing");
+	    done = createTaskColumn("Done");
+        
+        HBox hbox = new HBox(todo, doing, done);
+        hbox.setAlignment(Pos.CENTER);
 
 		return hbox;
 	}
+	
+	public BorderPane createTaskColumn(String title) {
+		BorderPane base = new BorderPane();
+		base.setId(title);
+		
+		HBox topLabel = new HBox(); 
+		Label lb = new Label(title);
+		lb.setFont(new Font("Arial", 15));
+		
+		
+		topLabel.getChildren().addAll(lb);
+		topLabel.setAlignment(Pos.CENTER);
+		topLabel.setSpacing(10);
+		topLabel.setPadding(new Insets(5,20,0,20));
+		
+		String colLayout = "-fx-border-color: black;\n" +
+                "-fx-border-insets: 5;\n" +
+                "-fx-border-width: 1;\n" +
+                "-fx-border-style: solid;\n";
+		
+		HBox botLabel = new HBox();
+		Button add = new Button("Add Task"); 
 
+		
+		Task t = new Task();
+		EventHandler addHandler = t.addTaskClicked(base.getId());
+	
+		add.setOnAction(addHandler); 
+		
+		Button delete = new Button("Delete Task");
+		
+		delete.setOnAction(e -> deleteButtonClicked(base.getId()));
+		
+		botLabel.setAlignment(Pos.CENTER);
+		
+		add.setMaxWidth(Double.MAX_VALUE);
+		delete.setMaxWidth(Double.MAX_VALUE);
+		
+		botLabel.setSpacing(10);
+		botLabel.getChildren().addAll(add,delete);
+		
+		// list 
+		TaskList tl = new TaskList();
+		ListView<String> lvList = tl.constructList();
+	
+		base.setTop(topLabel);
+		base.setCenter(lvList);
+		base.setBottom(botLabel);
+		
+		base.setStyle(colLayout);
+		
+		return base;
+		
+	}
+	
+	public static BorderPane getBorderPane(String id) {
+		if(id == "ToDo") {
+			return todo;
+		} else if (id == "Doing") {
+			return doing;
+		} else {
+			return done;
+		}
+	}
+
+	public void deleteButtonClicked(String id) {
+		BorderPane bp = getBorderPane(id);
+		ListView<String> lvList = (ListView<String>) bp.getChildren().get(1);
+		
+		// get id of selected item to delete 
+		int selectedID = lvList.getSelectionModel().getSelectedIndex();
+		
+		// remove the item from the correct list 
+		lvList.getItems().remove(selectedID);
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
